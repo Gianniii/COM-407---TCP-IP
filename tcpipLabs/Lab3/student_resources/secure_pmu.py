fe66bca9cdc79ca9ad5d8b1ada003146c3566166
gianniii <gianni.lodetti@hotmail.com>
2021-11-13 19:45:36 +0100

    part5 started

---
+++ tcpipLabs/Lab3/student_resources/secure_pmu.py
@@ -0,0 +1,36 @@
+ 
+#!/usr/bin/python
+ 
+import sys
+import socket
+import ssl
+ 
+ 
+CERTIFICATE = sys.argv[1]
+KEY = sys.argv[2]
+HOST = "localhost"
+PORT = 5003
+ 
+
+
+
+ 
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind((HOST, PORT))
+sock.listen(10)
+
+newSocket, addr = sock.accept()
+
+newSocket = ssl.wrap_socket(newSocket, server_side=True, certfile = CERTIFICATE, keyfile=KEY)
+newSocket.bind((HOST, PORT))
+
+COMMAND = newSocket.listen()
+ 
+while True: 
+    data = newSocket.recv(1024)
+    if not data: 
+        break
+    if(data.decode() == "CMD_SHORT:0"):
+        newSocket.send("This is PMU data 0")
+
+newSocket.close()
