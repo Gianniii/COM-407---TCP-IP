5d8293b2650cb754e8310069f35ddffe1b6dde59
Gianni <gianni.lodetti@hotmail.com>
2021-11-17 22:51:42 +0100

    finished

--- tcpipLabs/Lab3/student_resources/secure_pmu.py
+++ tcpipLabs/Lab3/student_resources/secure_pmu.py
@@ -6,31 +6,31 @@ import socket
 import ssl
  
  
-CERTIFICATE = sys.argv[1]
-KEY = sys.argv[2]
+CERTIFICATE = sys.argv[1] #certchain path .pem
+KEY_PATH = sys.argv[2] #.key
 HOST = "localhost"
 PORT = 5003
  
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+context.load_cert_chain(CERTIFICATE, KEY_PATH)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as sock:
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind((HOST, PORT))
+    sock.listen(5) #not sure what number in here is
+    with context.wrap_socket(sock, server_side=True) as ssock:
+        conn, addr = ssock.accept()
+    
 
+        print("enter loop")
+        command = ""
+        while True: 
+            data = conn.recv(15) 
+            #obv would need to do more procesisng in case we receive the command ints several packets..
+            print(data.decode())
+            if not data: 
+                break
+            if(data.decode() == "CMD_short:0"):
+                print("sending reply")
+                conn.send(("This is PMU data 0").encode())
 
-
- 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind((HOST, PORT))
-sock.listen(10)
-
-newSocket, addr = sock.accept()
-
-newSocket = ssl.wrap_socket(newSocket, server_side=True, certfile = CERTIFICATE, keyfile=KEY)
-newSocket.bind((HOST, PORT))
-
-COMMAND = newSocket.listen()
- 
-while True: 
-    data = newSocket.recv(1024)
-    if not data: 
-        break
-    if(data.decode() == "CMD_SHORT:0"):
-        newSocket.send("This is PMU data 0")
-
-newSocket.close()
+        conn.close()
